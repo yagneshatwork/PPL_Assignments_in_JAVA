df534c40bf9402ecd6d9be209a6b0c85a021e94b
YagneshAtWork <yagneshatwork20@gmail.com>
2026-01-14 00:52:59 +0530

    Add files via upload

---
+++ ArrayPuzzle.java
@@ -0,0 +1,112 @@
+class ArrayPuzzle
+{
+	static void MissingElements(int[] arr,int n)
+	{
+		int arr_sum = 0;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			arr_sum += arr[i];
+		}
+		
+		int n_sum = (n*(n-1))/2;
+		
+		System.out.println("Missing Element is : "+(n_sum-arr_sum)+"\n");
+	}
+	
+	static void Min_Max(int[] arr)
+	{
+		int min = arr[0];
+		int max = arr[0];
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			if(min>arr[i])
+			{
+				min=arr[i];
+			}
+			if(max<arr[i])
+			{
+				max=arr[i];
+			}
+		}
+		
+		System.out.println("Minimum Element = "+min+"\n");
+		System.out.println("Maximum Element = "+max+"\n");
+	}
+		
+	static void Sort(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int k = arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=k;
+				}
+			}
+		}
+		
+		System.out.print("Sorted Array = ");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("\n");
+	}
+	
+	static void Search(int[] arr,int find)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==find)
+			{
+				System.out.println(find+" Element Found!!"+"\n");
+				return;
+			}
+		}
+		System.out.println(find+" Element not Found");
+	}
+	
+	static void SumPairs(int[] arr,int f_add)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0+i;j<arr.length;j++)
+			{
+				if(arr[i]!=arr[j])
+				{
+					if(f_add==arr[i]+arr[j])
+					{
+						System.out.println("Sum Pair of "+f_add+" is : ("+arr[i]+","+arr[j]+")");
+					}
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr={4,1,6,3,5};
+		int n=6;
+		int find=3;
+		int f_add=6;
+		
+		System.out.print("Array = ");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("\n");
+		
+		MissingElements(arr,n);
+		Min_Max(arr);
+		Sort(arr);
+		Search(arr,find);
+		SumPairs(arr,f_add);
+	}
+		
+}
---
+++ display.java
@@ -0,0 +1,88 @@
+class student
+{
+	private String studentID;
+	private String name;
+	private float oop,ds,deco,mfg;
+		
+	public void setStudentID(String studentID)  //set student id
+	{
+		this.studentID=studentID;
+	}
+	
+	public void setName(String name)  //set name
+	{
+		this.name=name;
+	}
+	
+	public void setMarks(float oop,float ds,float deco,float mfg)  //set marks
+	{
+		this.oop=oop;
+		this.ds=ds;
+		this.deco=deco;
+		this.mfg=mfg;
+	}
+	
+	public String getStudentID()  //get student id
+	{
+		return studentID;
+	}
+	
+	public String getName()  //get name
+	{
+		return name;
+	}
+	
+	public float getAverage() //get average
+	{
+		float total = ((oop + ds + deco + mfg)/400)*100;
+		return total;
+    }
+		
+	public void studentdisplay()  //display
+	{
+		float avg=getAverage();
+		
+		System.out.println("Student ID : "+studentID+"\n");
+		System.out.println("Name :"+name+"\n");
+		System.out.println("Marks : OOP  ="+oop);
+		System.out.println("      : DS   ="+ds);
+		System.out.println("      : DECO ="+deco);
+		System.out.println("      : MFG  ="+mfg+"\n");
+			
+		if(oop<0||ds<0||deco<0||mfg<0)
+		{
+			System.out.println("Marks cannot be less then 0 or negative");
+		}
+		else if(oop>100||ds>100||deco>100||mfg>100)
+		{
+			System.out.println("Marks cannot be greater then 100");
+		}
+		else
+		{
+			System.out.println("Average :"+avg+"%\n");
+			
+			if(oop<50||ds<50||deco<50||mfg<50)
+			{
+				System.out.println("Result = Fail❌");
+			}
+			else
+			{
+				System.out.println("Result = Pass✅");
+			}
+		}
+	}
+}
+
+class display
+{
+	public static void main(String[] args)  //main
+	{
+		student d = new student();
+		
+		d.setStudentID("B24CE1952");
+		d.setName("Yagnesh");
+		d.setMarks(55,-63,72,87);
+		d.studentdisplay();
+	}
+}
+		
