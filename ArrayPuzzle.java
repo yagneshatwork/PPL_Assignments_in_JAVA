edafa41a5d50d3b5e37857ffbb1148d6f98f81a8
YagneshAtWork <yagneshatwork20@gmail.com>
2026-01-19 18:32:07 +0530

    Fis in MergeNSort Function

--- ArrayPuzzle.java
+++ ArrayPuzzle.java
@@ -1,6 +1,6 @@
 class ArrayPuzzle
 {
-	static void MissingElements(int[] arr,int n)
+	static void MissingElements(int[] arr,int n)  //To find missing elements in arr
 	{
 		int arr_sum = 0;
 		
@@ -14,7 +14,7 @@ class ArrayPuzzle
 		System.out.println("Missing Element is : "+(n_sum-arr_sum)+"\n");
 	}
 	
-	static void Min_Max(int[] arr)
+	static void Min_Max(int[] arr)  //To find the minimum and maximum
 	{
 		int min = arr[0];
 		int max = arr[0];
@@ -35,7 +35,7 @@ class ArrayPuzzle
 		System.out.println("Maximum Element = "+max+"\n");
 	}
 		
-	static void Sort(int[] arr)
+	static void Sort(int[] arr)  //Bubble sort
 	{
 		for(int i=0;i<arr.length;i++)
 		{
@@ -58,7 +58,7 @@ class ArrayPuzzle
 		System.out.println("\n");
 	}
 	
-	static void Search(int[] arr,int find)
+	static void Search(int[] arr,int find)  //Search an element in arr
 	{
 		for(int i=0;i<arr.length;i++)
 		{
@@ -68,10 +68,10 @@ class ArrayPuzzle
 				return;
 			}
 		}
-		System.out.println(find+" Element not Found");
+		System.out.println(find+" Element not Found\n");
 	}
 	
-	static void SumPairs(int[] arr,int f_add)
+	static void SumPairs(int[] arr,int f_add)  //To fins sum pairs of given elemrent
 	{
 		for(int i=0;i<arr.length;i++)
 		{
@@ -86,9 +86,63 @@ class ArrayPuzzle
 				}
 			}
 		}
+		System.out.println();
 	}
 	
-	public static void main(String[] args)
+	static void MergeNSort(int[] brr, int[] crr)  // //To merge n sort brr & crr simultaneously
+	{
+		int[] merge = new int[brr.length + crr.length];
+		boolean[] usedB = new boolean[brr.length];
+		boolean[] usedC = new boolean[crr.length];
+
+		for (int i = 0; i < merge.length; i++)
+		{
+			int min = 9999;
+			int index = -1;
+			char from = ' ';
+
+			// find smallest unused element in brr
+			for (int j = 0; j < brr.length; j++)
+			{
+				if (!usedB[j] && brr[j] < min)
+				{
+					min = brr[j];
+					index = j;
+					from = 'B';
+				}
+			}
+
+			// find smallest unused element in crr
+			for (int j = 0; j < crr.length; j++)
+			{
+				if (!usedC[j] && crr[j] < min)
+				{
+					min = crr[j];
+					index = j;
+					from = 'C';
+				}
+			}
+
+			// insert smallest into merged array
+			merge[i] = min;
+
+			// mark element as used
+			if (from == 'B')
+				usedB[index] = true;
+			else
+				usedC[index] = true;
+		}
+
+    System.out.print("Merged N Sorted Array = ");
+    for (int i = 0; i < merge.length; i++)
+    {
+        System.out.print(merge[i] + " ");
+    }
+    System.out.println();
+}
+
+	
+	public static void main(String[] args)  //Main function
 	{
 		int[] arr={4,1,6,3,5};
 		int n=6;
@@ -110,6 +164,24 @@ class ArrayPuzzle
 		Sort(arr);
 		Search(arr,find);
 		SumPairs(arr,f_add);
+		
+		int[] brr={5,8,3,7};
+		int[] crr={6,9,2,0,4,1};
+		
+		System.out.print("brr = ");
+		for(int i=0;i<brr.length;i++)
+		{
+			System.out.print(brr[i]+" ");
+		}
+		System.out.println();
+		System.out.print("crr = ");
+		for(int i=0;i<crr.length;i++)
+		{
+			System.out.print(crr[i]+" ");
+		}
+		System.out.println("\n");
+		
+		MergeNSort(brr,crr);
 	}
 		
 }
